d666fd59c5d954ecb48ba2815370ea574611fcce
Shlok Arora <128911480+ShlokArora2709@users.noreply.github.com>
2023-11-20 22:10:11 +0530

    Enhanced code readability, added comments, and improved visualization in confusion matrix heatmap.

--- data_science_pr/random_forest.py
+++ data_science_pr/random_forest.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import pandas as pd
 import numpy as np
 from sklearn.datasets import load_digits
@@ -9,34 +10,37 @@ import seaborn as sn
 
 # Load the digits dataset
 digits = load_digits()
-df= pd.DataFrame(digits.data)
-df['target']=digits.target
-
+df = pd.DataFrame(digits.data)
+df['target'] = digits.target
 
 def split():
-    X=df.drop(['target'],axis=1)
-    Y=df['target']
+    # Split the dataset into features (X) and target variable (Y)
+    X = df.drop(['target'], axis=1)
+    Y = df['target']
     global X_train, X_test, y_train, y_test
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
 def train():
     global model
-    model=RandomForestClassifier(n_estimators=50)
-    model.fit(X_train,y_train)
-    model.score(X_test,y_test)
+    # Initialize and train the RandomForestClassifier
+    model = RandomForestClassifier(n_estimators=50)
+    model.fit(X_train, y_train)
+    # Print the accuracy score on the test set
+    print("Accuracy:", model.score(X_test, y_test))
 
 def plot_g():
-    y_pred=model.predict(X_test)
-    cm =confusion_matrix(y_true=y_test,y_pred=y_pred)
-    plt.figure(figsize=(10,7))
+    y_pred = model.predict(X_test)
+    # Generate and display the confusion matrix heatmap
+    cm = confusion_matrix(y_true=y_test, y_pred=y_pred)
+    plt.figure(figsize=(10, 7))
     sn.heatmap(cm, annot=True, fmt='d', cmap='Blues', annot_kws={"size": 12})
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted Labels')
     plt.ylabel('Actual Labels')
     plt.show()
 
-
-
+# Execute the functions
 split()
 train()
 plot_g()
