a817a378d90c1a351473d7da609dfe94b2db1e2d
Shlok Arora <shlokarora2709@gmail.com>
2024-01-13 18:17:22 +0530

    finished Leetcode Pandas introduction study plan

--- IntroToPandas_LeetCode/PandasBasics.py
+++ IntroToPandas_LeetCode/PandasBasics.py
@@ -20,3 +20,36 @@ def selectData(students: pd.DataFrame) -> pd.DataFrame:
 def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
     employees['bonus']=employees['salary']*2
     return employees
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    return customers.drop_duplicates(subset=['email'])
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.dropna(subset=['name'])
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['salary']=employees['salary']*2
+    return employees
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    return students.rename(columns={'id': 'student_id', 'first': 'first_name', 'last': 'last_name', 'age': 'age_in_years'})
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students['grade']=students['grade'].astype(int)
+    return students
+
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    products['quantity'].fillna(0, inplace=True)
+    return products
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2])
+
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+    return weather.pivot_table(index='month', columns='city', values='temperature',aggfunc='max')
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return report.melt(id_vars='product',var_name='quarter', value_name='sales')
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    return animals[animals['weight']>100].sort_values(by="weight",ascending=False)[['name']]
